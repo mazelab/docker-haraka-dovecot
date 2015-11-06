ae4ee9e251231b5c682a87aed67a2f4c34ac7daf
Marcel Kilian <cdskilian@gmail.com>
2015-11-06 17:01:25 +0100

    current dev state

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,39 @@
 FROM mhart/alpine-node:0.12.7
 
-RUN apk add --update openssl dovecot dovecot-mysql python spamassassin supervisor make g++ wget
+RUN apk add --update dovecot dovecot-mysql spamassassin supervisor openssl python make g++ wget ca-certificates bash logrotate && \
+      npm install -g Haraka@2.7.0 mkdirp mysql && \
+      mkdir -p /tmp/haraka-plugins /default/haraka-plugins /default/dovecot /data && chown mail: /data && \
+      sa-update && \
+      wget -O /tmp/haraka-plugins.zip https://github.com/mazelab/haraka-plugins/archive/master.zip && \
+      unzip /tmp/haraka-plugins.zip -d /tmp && \
+      cp -R /tmp/haraka-plugins-master/* /default/haraka-plugins/. && \
+      wget -O /default/haraka-plugins/maildir.js https://raw.githubusercontent.com/madeingnecca/haraka-plugins/master/maildir.js && \
+      apk del --purge openssl python make g++ wget && \
+      rm -r /tmp/* /etc/dovecot/*
 
-# install haraka
-RUN npm install -g Haraka mkdirp mysql
-RUN mkdir /srv && haraka -i /srv/haraka && mkdir /srv/haraka/queue
+ENV HARAKA_DIR /srv/haraka
+ENV HARAKA_DEFAULT_PLUGINS /default/haraka-plugins
+ENV HARAKA_DEFAULT_CONFIG /usr/lib/node_modules/Haraka/config
+ENV DOVECOT_DEFAULT /default/dovecot
 
-# haraka config
-ADD haraka/plugins /srv/haraka/plugins
-ADD haraka/config /srv/haraka/config
+ENV DOVECOT_QUERY_USER "SELECT maildir, 8 AS uid, 12 AS gid, concat('*:storage=', quota, 'M') AS quota_rule FROM users WHERE email = '%u'"
+ENV DOVECOT_QUERY_PASS "SELECT email AS user, password as password, maildir as userdb_home, 8 AS userdb_uid, 12 AS userdb_gid FROM users WHERE email = '%u'"
 
-# dovecot config
-ADD dovecot/ /etc/dovecot/
-
-# spamassassin config
-RUN sa-update
-
-# maildirs config
-RUN mkdir /data/ && chown mail:mail -R /data/ /srv/haraka/queue/
+# copy custom config
+COPY dovecot/ $DOVECOT_DEFAULT
+COPY haraka $HARAKA_DEFAULT_CONFIG
+COPY logrotate.d/ /etc/logrotate.d/
 
 # add starter script
-ADD supervisord.ini /etc/supervisor.d/supervisord.ini
-ADD run.sh /usr/local/bin/run
-RUN chmod +x /usr/local/bin/run
+COPY supervisord.ini /etc/supervisor.d/supervisord.ini
+COPY docker-entrypoint.sh /entrypoint.sh
 
 EXPOSE 25 110 143 993 995
 
-VOLUME ["/data/", "/tls/"]
+#@todo change dovecot uid to < 1000
+#@todo improve spamassassin logging
+VOLUME ["/srv/haraka/", "/data/", "/etc/dovecot/", "/var/log/"]
 
-CMD ["/usr/local/bin/run"]
+ENTRYPOINT ["/entrypoint.sh"]
+
+CMD ["supervisord"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+set -e
+
+function initHaraka() {
+  # install haraka config
+  if [ ! -d "$HARAKA_DIR/config" ] ; then
+    echo "installing haraka config..."
+    cp -R $HARAKA_DEFAULT_CONFIG $HARAKA_DIR/config
+  fi
+
+  # install haraka plugins
+  if [ ! -d "$HARAKA_DIR/plugins" ] ; then
+    echo "installing haraka plugins..."
+
+    # add plugin files
+    mkdir -p $HARAKA_DIR/plugins/auth
+    cp $HARAKA_DEFAULT_PLUGINS/*.js $HARAKA_DIR/plugins/.
+
+    # add plugin config
+    cp $HARAKA_DEFAULT_PLUGINS/config/* $HARAKA_DIR/config/.
+
+    if [ -f "$HARAKA_DIR/plugins/auth_mysql_cryptmd5.js" ]; then
+      mv $HARAKA_DIR/plugins/auth_mysql_cryptmd5.js $HARAKA_DIR/plugins/auth/.
+    fi
+    if [ -f "$HARAKA_DIR/plugins/cryptmd5.js" ]; then
+      mv $HARAKA_DIR/plugins/cryptmd5.js $HARAKA_DIR/plugins/auth/.
+    fi
+  fi
+
+  # create haraka queue folder
+  if [ ! -d "$HARAKA_DIR/queue" ] ; then
+    mkdir $HARAKA_DIR/queue
+  fi
+
+  hostname -f > $HARAKA_DIR/config/me
+  hostname -f > $HARAKA_DIR/config/host_list
+
+  # setup haraka mysql stuff
+  if [ ! -z "$MYSQL_HOST" ]; then
+    sed -i "s/^host=.*/host=$MYSQL_HOST/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$MYSQL_USER" ]; then
+    sed -i "s/^user=.*/user=$MYSQL_USER/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$MYSQL_PASS" ]; then
+    sed -i "s/^password=.*/password=$MYSQL_PASS/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$MYSQL_DATABASE" ]; then
+    sed -i "s/^database=.*/database=$MYSQL_DATABASE/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$MYSQL_PORT" ]; then
+    sed -i "s/^port=.*/port=$MYSQL_PORT/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$MYSQL_CHARSET" ]; then
+    sed -i "s/^char_set=.*/char_set=$MYSQL_CHARSET/g" $HARAKA_DIR/config/mysql_provider.ini
+  fi
+  if [ ! -z "$HA_QUERY_ALIASES" ]; then
+    sed -i "s/^query=.*/query=$HA_QUERY_ALIASES/g" $HARAKA_DIR/config/aliases_mysql.ini
+  fi
+  if [ ! -z "$HA_QUERY_AUTH" ]; then
+    sed -i "s/^query=.*/query=$HA_QUERY_AUTH/g" $HARAKA_DIR/config/auth_mysql_cryptmd5.ini
+  fi
+  if [ ! -z "$HA_QUERY_QUOTA" ]; then
+    sed -i "s/^query=.*/query=$HA_QUERY_QUOTA/g" $HARAKA_DIR/config/quota_mysql.ini
+  fi
+  if [ ! -z "$HA_QUERY_RCPT_TO" ]; then
+    sed -i "s/^query=.*/query=$HA_QUERY_RCPT_TO/g" $HARAKA_DIR/config/rcpt_to.mysql.ini
+  fi
+}
+
+function initDovecot() {
+  if [ -z "$(ls -A /etc/dovecot)" ]; then
+    echo "installing dovecot config..."
+    cp -R $DOVECOT_DEFAULT/* /etc/dovecot/.
+  fi
+
+  if [ ! "`cat /etc/dovecot/dovecot-sql.conf.ext | grep '^connect'`" ]; then
+    echo "connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASS" >> /etc/dovecot/dovecot-sql.conf.ext
+  fi
+  if [ ! "`cat /etc/dovecot/dovecot-dict-sql.conf.ext | grep '^connect'`" ]; then
+    echo "connect = host=$MYSQL_HOST dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASS" >> /etc/dovecot/dovecot-dict-sql.conf.ext
+  fi
+
+  if [ "$DOVECOT_QUERY_USER" -a ! "`cat /etc/dovecot/dovecot-sql.conf.ext | grep '^user_query ='`" ]; then
+    echo "user_query = $DOVECOT_QUERY_USER" >> /etc/dovecot/dovecot-sql.conf.ext
+  fi
+  if [ "$DOVECOT_QUERY_PASS" -a ! "`cat /etc/dovecot/dovecot-sql.conf.ext | grep '^password_query ='`" ]; then
+    echo "password_query = $DOVECOT_QUERY_PASS" >> /etc/dovecot/dovecot-sql.conf.ext
+  fi
+}
+
+function initTLS() {
+  if [ ! -f "$HARAKA_DIR/config/tls_cert.pem" ]; then
+    echo "Installing self signed ssl cert"
+    openssl req -x509 -nodes -days 2190 -newkey rsa:2048 \
+            -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
+            -keyout $HARAKA_DIR/config/tls_key.pem -out $HARAKA_DIR/config/tls_cert.pem
+  fi
+
+  if [ ! -s "/etc/dovecot/conf.d/10-ssl.conf" ]; then
+    echo "ssl      = yes" >> /etc/dovecot/conf.d/10-ssl.conf
+    echo "ssl_cert = <$HARAKA_DIR/config/tls_cert.pem" >> /etc/dovecot/conf.d/10-ssl.conf
+    echo "ssl_key  = <$HARAKA_DIR/config/tls_key.pem"  >> /etc/dovecot/conf.d/10-ssl.conf
+  fi
+}
+
+function fixPermissions() {
+  #secure cert permissions
+  if [ -f "$HARAKA_DIR/config/tls_cert.pem" ]; then
+    chmod 444 $HARAKA_DIR/config/tls_cert.pem
+  fi
+  if [ -f "$HARAKA_DIR/config/tls_key.pem" ]; then
+    chmod 400 $HARAKA_DIR/config/tls_key.pem
+  fi
+
+  # map permissions to the user who mounted /data
+  DATA_UID=`ls -ldn "/data" | awk '{print $3}'`
+  MAPPED_USER=`getent passwd $DATA_UID | awk -F':' '{print $1}'`
+  MAIL_USER="mail"
+
+  if [ -z "$MAPPED_USER" ]; then
+    echo "Map mail user to $DATA_UID"
+    usermod -u $DATA_UID $MAIL_USER
+  elif [ "$MAPPED_USER" != "$MAIL_USER" ]; then
+    echo "system users ($MAPPED_USER) except mail should not own data. Changing ownership of /data/..."
+    chown -R mail: /data
+  fi
+
+  # set queue ownership
+  chown -R mail: $HARAKA_DIR/queue
+}
+
+initHaraka
+initDovecot
+initTLS
+fixPermissions
+
+# runtime configuration in order to support mounts
+if [[ "$*" == supervisord* ]]; then
+  echo "supervisord"
+fi
+
+exec "$@"
